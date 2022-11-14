0e31d775effec780d32bc84bc09badf3393c899c
BT_4N2_17 <lsarmiento9311@bths.edu>
2022-11-14 12:34:29 -0500

    finally decided my constructors.

--- src/RunFromFate.java
+++ src/RunFromFate.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class RunFromFate {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner fate = new Scanner(System.in);
+        TimeOfFate time = new TimeOfFate();
+        System.out.println(time.classAttribute());
     }
 }
--- src/TimeOfFate.java
+++ src/TimeOfFate.java
@@ -1,2 +1,42 @@
 public class TimeOfFate {
+    private String classInLife;
+
+    public TimeOfFate(String classInLife){
+        this.classInLife = classInLife;
+    }
+
+    public TimeOfFate(){
+        int random = (int)(Math.random()*3)+1;
+        if(random == 1){
+            classInLife = "Royalty";
+        }
+        else if(random == 1){
+            classInLife = "Commoner";
+        }
+        else{
+            classInLife = "Peasant";
+        }
+    }
+
+    public String classAttribute(){
+        if(classInLife == "Royalty"){
+            return "You are royalty!";
+        }
+        else{
+            return "you suck";
+        }
+    }
+    int currentYear = 1880;
+    public void rewindTime(int year) throws InterruptedException {
+        int ms = 1000;
+        for(int i = currentYear; i>=year; i--){
+            System.out.println("Year: " + i);
+            Thread.sleep(ms);
+            if(ms>100){
+                ms = ms - 100;
+            }
+        }
+
+    }
 }
+
